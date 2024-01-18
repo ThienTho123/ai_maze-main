d2ff409dfa66cd7ce54b1539d6d0ae5402a99b47
ThienTho123 <thientho7a4@gmail.com>
2024-01-18 20:01:35 +0700

    test

---
+++ mytest/menuTest.py
@@ -0,0 +1,67 @@
+import pygame, sys
+
+# Setup pygame/window ---------------------------------------- #
+from pygame.locals import *
+
+from menu.menu import main_menu
+
+mainClock = pygame.time.Clock()
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500), 0, 32)
+
+font = pygame.font.SysFont(None, 20)
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+def main():
+    click = False
+    while True:
+        screen.fill((0, 0, 0))
+        # click = main_menu(screen, click, callback={"x": game()})
+        click = main_menu(screen, click, [game, options])
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+def game():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        draw_text('game', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+def options():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+main()
