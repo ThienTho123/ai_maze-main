f9867db8a297265b9826adc0a1a8a24d9cbc20f3
ThienTho123 <thientho7a4@gmail.com>
2024-01-18 20:00:10 +0700

    add config

---
+++ config/config.py
@@ -0,0 +1,30 @@
+HD = (1280, 720)
+FHD = (1920, 1080)
+QHD = (2560, 1440)
+UHD = (3840, 2160)
+RUHD = (4096, 2160)
+
+# COLORS (r, g, b)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 255, 255)
+DARKGREY = (40, 40, 40)
+CELLCOLOR = (38, 50, 56)
+BGCOLOUR = DARKGREY
+
+# setings
+title = "AI MAZE Game"
+
+# game settings
+CUSTOM_CONFIG = False
+
+MAINCOLOR = 'darkslategray'
+STARTCOLOR = 'saddlebrown'
+VISITEDCOLOR = 'black'
+BORDERCOLOR = 'darkorange'
+INIT_WIDTH = 1000
+INIT_HEIGHT = 641
+FPS = 60
+CELLSIZE = 50
+
+Visualization_speed = 20
